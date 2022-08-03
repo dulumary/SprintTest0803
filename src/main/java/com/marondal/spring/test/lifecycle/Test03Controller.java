83861dc773b9315b6923717302fad4b5afb4f831
asdf <asdf@gmail.com>
2022-08-03 17:53:57 +0900

    [김인규]  - jsp 연동 문제 풀이

---
+++ src/main/java/com/marondal/spring/test/lifecycle/Test03Controller.java
@@ -0,0 +1,14 @@
+package com.marondal.spring.test.lifecycle;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Test03Controller {
+	
+	@RequestMapping("/lifecycle/test03/1")
+	public String test03() {
+		return "lifecycle/test03";
+	}
+
+}
